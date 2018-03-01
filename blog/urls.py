2140307b71d354aa42c8df444b86ac34fcdd4cb3
scarmody402 <scarmody@unomaha.edu>
2018-03-01 08:27:49 -0600

    small stuff

--- blog/urls.py
+++ blog/urls.py
@@ -3,13 +3,11 @@ from django.contrib.auth import views as auth_views
 from django.conf.urls import url
 from . import views
 
-
 urlpatterns = [
-    url(r'^login/$', auth_views.login, name='login'),
-    url(r'^admin/', admin.site.urls),
     url(r'^login/$', auth_views.login, {'template_name': 'blog/login.html'}, name='login'),
     url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
     url(r'^signup/$', views.signup, name='signup'),
+    url(r'^admin/', admin.site.urls),
     url(r'^$', views.post_list, name='post_list'),
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
     url(r'^post/new/$', views.post_new, name='post_new'),
